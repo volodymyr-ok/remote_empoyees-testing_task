1fd478e673fe4178aa317d60c5f6528fa17bca0b
Volodymyr Ok <v.shark.t@gmail.com>
2023-04-08 01:35:09 +0300

    changed logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,13 +4,27 @@ import { colors } from '../../utils/repeatStyles';
 import { Link } from 'react-router-dom';
 
 const LogoLink = styled(Link)`
+  position: relative;
+
   font-size: 24px;
   font-weight: bold;
   color: ${colors.minorText};
+  :after {
+    position: absolute;
+    bottom: 5px;
+    right: -22px;
+
+    content: '';
+    display: block;
+    width: 18px;
+    height: 2px;
+    background-color: ${colors.minorText};
+    /* margin-top: 10px; */
+  }
 `;
 
 const Logo = () => {
-  return <LogoLink to="./">trppd_</LogoLink>;
+  return <LogoLink to="./">trppd</LogoLink>;
 };
 
 export default Logo;
